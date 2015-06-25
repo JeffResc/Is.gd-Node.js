3ec6d4b9f6c2d1a70e944d368eb6dd8eaab3b2a2
AlphaTech <alphatech@btri.x10.bz>
2015-06-24 23:45:51 -0400

    Fix URL Lookup

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ module.exports = {
         request('http://is.gd/create.php?format=simple&url=' + url, function (error, response, body) {
 			cb(body.split("\n")[0]);
         });
-    }
+    },
     lookup: function(url, cb) {
-        request('http://is.gd/forward.php?shorturl=' + url, function (error, response, body) {
+        request('http://is.gd/forward.php?format=simple&shorturl=' + url, function (error, response, body) {
 			cb(body.split("\n")[0]);
         });
     }
