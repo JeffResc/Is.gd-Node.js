0bb22c6e2f67ee76c73d20e42ffbeb8c32f560eb
Hugo Durães <hugoduraes@gmail.com>
2016-10-04 00:00:08 +0100

    remove dependencies

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-var request = require('request');
+var https = require('https');
 
 module.exports = {
     shorten: function(url, cb) {
-        request('http://is.gd/create.php?format=simple&url=' + encodeURIComponent(url), function (error, response, body) {
-		cb(body.split("\n")[0]);
+        https.get('https://is.gd/create.php?format=simple&url=' + encodeURIComponent(url), function (res) {
+            var body = '';
+            res.on('data', function(chunk) { body += chunk; });
+            res.on('end', function() { cb(body); });
         });
     },
     custom: function(url, text, cb) {
-	request('http://is.gd/create.php?format=simple&url=' + encodeURIComponent(url) + '&shorturl=' + encodeURIComponent(text), function (error, response, body) {
-		cb(body.split("\n")[0]);
-	});
+        https.get('https://is.gd/create.php?format=simple&url=' + encodeURIComponent(url) + '&shorturl=' + encodeURIComponent(text), function (res) {
+            var body = '';
+            res.on('data', function(chunk) { body += chunk; });
+            res.on('end', function() { cb(body); });
+        });
     },
     lookup: function(url, cb) {
-        request('http://is.gd/forward.php?format=simple&shorturl=' + encodeURIComponent(url), function (error, response, body) {
-		cb(body.split("\n")[0]);
+        https.get('https://is.gd/forward.php?format=simple&shorturl=' + encodeURIComponent(url), function (res) {
+            var body = '';
+            res.on('data', function(chunk) { body += chunk; });
+            res.on('end', function() { cb(body); });
         });
     }
 };
